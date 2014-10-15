3749189ea11e6eef29081bbc18e119d9199d6727
Sean Dawson <contact@seandawson.info>
2014-10-15 15:20:49 -0300

    Made the check for missing values actually take the value into account, not just the existance of a key.

--- libs/utils.js
+++ libs/utils.js
@@ -6,13 +6,21 @@
   var logger = require('./logger.js');
 
   module.exports = {
-    validateRequiredOptions: function (object, keys) {
+    validateRequiredOptions: function (object, keys, strict) {
+      strict = strict || true;
+
       if (!object && keys.length > 0) {
         return false;
       }
       _.each(keys, function (key) {
         if (!_.contains(Object.keys(object), key)) {
           throw new Error('Missing required option: ' + key);
+        }
+        var value = !object[key];
+        if (typeof(value) === 'undefined' ||
+          value === 'null' ||
+          (typeof(value) === 'string' && value.trim() === '')) {
+            throw new Error('Option is undefined, null or empty string: ' + key);
         }
       });
       return true;
